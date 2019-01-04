75524f6ed4b5cc6c99bca3f022ccf7fddc4da2df
Ean Daus <eandaus123@gmail.com>
2019-01-04 13:33:27 -0500

    Defined a function to print an array and an array for it to print

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+/*
+ * Ean Daus
+ * index.php
+ * 1/4/19
+ * Pair Program 1
+ */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+
+function printArray($array)
+{
+    echo implode($array, ", ");
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,5 +23,6 @@
 </head>
 <body>
     <h1>Pair Program 1</h1>
+    <?php printArray($numbers);?>
 </body>
 </html>
